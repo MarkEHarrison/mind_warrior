1597424ef2d790a0c69ba8cf862b320ee62e63db
Mark Harrison <mhworld11@icloud.com>
2019-06-20 18:20:16 -0700

    creates a sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     patch "/users/:id" => "users#update"
     delete "/users/:id" => "users#destroy"
 
+    post "/sessions" => "sessions#create"
+
+
     get "/user_meditations" => "user_meditations#index"
 
 
