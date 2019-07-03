b7b7a764781d0b17ac547343590a09b5e5697807
Mark Harrison <mhworld11@icloud.com>
2019-07-03 12:55:25 -0700

    latest version, no errors

--- app/views/api/meditations/_meditation.json.jbuilder
+++ app/views/api/meditations/_meditation.json.jbuilder
@@ -2,4 +2,4 @@ json.id meditation.id
 json.title meditation.title
 json.practice meditation.practice
 json.length meditation.length
-json.sound meditation.sound_url
+json.sound_url meditation.sound_url
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Meditation.create!([
-#   {}
-
-# ])
 
 
 
